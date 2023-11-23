322845be3bfb4b02d6887af8f5c23dc8dbb7987d
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-11-24 00:11:32 +0600

    Gulshan Pride Project Created with React,Rourer,Tailwind,Firebase and Initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <>
+      <h2>Hello</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Gulshan Pride</div>,
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
