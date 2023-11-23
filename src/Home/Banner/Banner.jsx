d2cbc8931d01c529c7789d894bd381708030791d
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-11-24 02:17:47 +0600

    Banner and About Section Added

---
+++ src/Home/Banner/Banner.jsx
@@ -0,0 +1,23 @@
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import b1 from '../../assets/b1.jpg'
+import b2 from '../../assets/b2.jpg'
+import b3 from '../../assets/b3.jpg'
+
+const Banner = () => {
+    return (
+        <Carousel autoPlay={true} infiniteLoop={true} className='max-w-screen-xl mx-auto text-center'>
+            <div>
+                <img src={b1} />
+            </div>
+            <div>
+                <img src={b2} />
+            </div>
+            <div>
+                <img src={b3} />
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,21 +1,12 @@
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import b1 from '../assets/b1.jpg'
-import b2 from '../assets/b2.jpg'
-import b3 from '../assets/b3.jpg'
+import About from "./About/About";
+import Banner from "./Banner/Banner";
+
 const Home = () => {
     return (
-        <Carousel autoPlay={true} infiniteLoop={true} className='max-w-screen-xl mx-auto text-center'>
-            <div>
-                <img src={b1} />
-            </div>
-            <div>
-                <img src={b2} />
-            </div>
-            <div>
-                <img src={b3} />
-            </div>
-        </Carousel>
+       <div>
+        <Banner></Banner>
+        <About></About>
+       </div>
     );
 };
 
